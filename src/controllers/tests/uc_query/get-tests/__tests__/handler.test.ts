096a2c070625bd7c982cca51d59e2c9950d7244d
quocanlikebanana <quocankg2611@gmail.com>
2025-06-28 16:18:55 +0700

    small fix 2

--- src/controllers/tests/uc_query/get-tests/__tests__/handler.test.ts
+++ src/controllers/tests/uc_query/get-tests/__tests__/handler.test.ts
@@ -1,8 +1,17 @@
+import { setupAfterAll, setupBeforeAll } from '../../../../../__tests__/integration/setup';
 import { GetTestsQueryHandler } from '../handler';
-import * as testData from './data';
+import * as testData from './input-data';
 
 describe('GetTestsQueryHandler (data-driven)', () => {
 	const handler = new GetTestsQueryHandler();
+
+	beforeAll(async () => {
+		await setupBeforeAll();
+	});
+
+	afterAll(async () => {
+		await setupAfterAll();
+	});
 
 	(testData.cases || []).forEach((testCase, idx) => {
 		it(testCase.name || `case #${idx + 1}`, async () => {
